d5e1cb3c4df25bf66d3623fe97a560e68801e7b2
SukhvirKooner <sukhvirkooner05@gmail.com>
2024-11-03 02:25:58 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,32 @@
+
+import axios from "axios";
+
+
+
+
+async function getUserDetails() {
+  // await new Promise((r)=>{setTimeout(r,5000)})
+  const response = await axios.get("http://localhost:3000/api/user")
+
+	return response.data;
+}
+// async component only in server component 
+// bcz this is a server component , this component gets server side rendered 
+export default async function Home() {
+  const userData = await getUserDetails();
+  
+
+  return (
+    <div className="flex flex-col justify-center h-screen">
+        <div className="flex justify-center">
+            <div className="border p-8 rounded">
+                <div>
+                    Name: {userData?.name}
+                </div>
+                
+                {userData?.email}
+            </div>
+        </div>
+    </div>
+  );
+}
---
+++ app/signup/page.tsx
@@ -0,0 +1,9 @@
+import { Signup } from "@/component/signup"
+
+export default function(){
+    return(
+        <div>
+           <Signup/>           
+        </div>
+    )
+}
